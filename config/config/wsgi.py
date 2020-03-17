1d0aee851c6977335c941dbc7d1079fb1cf625a9
Sankalpa Acharya <sanacharya000@gmail.com>
2020-03-17 10:40:54 +0545

    updated wsgi for python anywhere

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_wsgi_application()
+
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/sankalpa6969/projects/smartmotorcycle/config/smartmotorcycle'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'smartmotorcycle.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
